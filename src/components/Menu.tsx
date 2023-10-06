416d06b0f6f242ea4b0a22456b663b0ef680829b
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-10-06 21:45:55 +0100

    open menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+'use client'
+
+import React, {useState} from 'react'
 import Image from 'next/image'
 
 const Menu = () => {
+  const [open, setOpen] = useState(false)
   return (
     <div>
         <Image src="/open.png" alt="" width={20} height={20}/>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
-
 import Menu from './Menu'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div><Menu/></div>
+    <div>
+     
+      <div className="h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase">
+         {/*LOGO*/}
+         <div className='text-xl'>
+         <Link href="/">OrderApp</Link>
+         </div>
+        
+        <div>
+          {/*Mobile menu*/}
+          <div>
+            <Menu/>
+          </div>
+        </div>
+      </div>
+      
+      
+      
+      
+      </div>
   )
 }
 
