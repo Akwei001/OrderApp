879e30f821460f8ec24868069965338d7a59449e
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-10-09 21:15:57 +0100

    navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,11 @@ const Navbar = () => {
     <div>
      
       <div className="h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase">
+        <div className='hidden md:flex gap-4'>
+          <Link href="/">Homepage</Link>
+          <Link href="/menu">Menu</Link>
+          <Link href="/">Contact</Link>
+        </div>
          {/*LOGO*/}
          <div className='text-xl'>
          <Link href="/">OrderApp</Link>
@@ -14,7 +19,7 @@ const Navbar = () => {
         
         <div>
           {/*Mobile menu*/}
-          <div>
+          <div className='md:hidden'>
             <Menu/>
           </div>
         </div>
