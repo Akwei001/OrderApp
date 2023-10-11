fa16f286c75df7090ead53421518a6148137551f
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-10-11 20:29:50 +0100

    menu styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Menu from './Menu'
 import Link from 'next/link'
+import CartIcon from './CartIcon'
+import Image from 'next/image'
 
 const Navbar = () => {
+
+  const user = false
   return (
     <div>
      
       <div className="h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase">
+        {/*LEFT LINKS*/}
         <div className='hidden md:flex gap-4'>
           <Link href="/">Homepage</Link>
           <Link href="/menu">Menu</Link>
@@ -22,6 +27,16 @@ const Navbar = () => {
           <div className='md:hidden'>
             <Menu/>
           </div>
+        </div>
+        {/*RIGHT LINKS*/}
+        <div className='hidden md:flex gap-4'>
+          <div>
+            <Image src="/phone.png" alt="" width={20} height={20}/>
+            <span>555 123 456</span>
+          </div>
+         {!user ? (<Link href="/login">Login</Link>) :
+          (<Link href="/orders">Orders</Link>)}
+          <CartIcon/>
         </div>
       </div>
       
