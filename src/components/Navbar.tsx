4d9211f499f84d5b1b5e7b40f69bf921c0ea87fa
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-10-16 19:44:00 +0100

    navbar complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <div>
      
-      <div className="h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase md:h-24">
+      <div className="h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 lx:px-40">
         {/*LEFT LINKS*/}
         <div className='hidden md:flex gap-4 flex-1'>
           <Link href="/">Homepage</Link>
@@ -29,8 +29,8 @@ const Navbar = () => {
           </div>
         </div>
         {/*RIGHT LINKS*/}
-        <div className='hidden md:flex gap-4 items-center flex-1'>
-          <div className="md:absoulte top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+        <div className='hidden md:flex gap-4 items-center justify-end flex-1'>
+          <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
             <Image src="/phone.png" alt="" width={20} height={20}/>
             <span>555 123 456</span>
           </div>
