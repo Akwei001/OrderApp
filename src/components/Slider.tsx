021b1cc32a53f49e896ca3874374855ce73b7c5b
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-10-24 21:45:52 +0100

    slider usestate working

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
 import { useState } from 'react'
@@ -23,17 +25,18 @@ const data = [
 ]
 
 const Slider = () => {
-    const [currentSlide, setCurrentSlide] = useState(0)
+    const [currentSlide, setCurrentSlide] = useState(1)
   return (
     <div className=" flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]">
         {/*TEXT CONTAINER*/}
         <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
-            <h1 className="text-5xl uppercase p-4 text-center md:text-6xl xl:text-7xl">Test</h1>
+            <h1 className="text-5xl uppercase p-4 text-center md:text-6xl xl:text-7xl">{data[currentSlide].title}
+</h1>
             <button className="bg-red-500 text-white py-4 px-8 ">Order Now</button>
         </div>
         {/*IMAGE CONTAINER*/}
         <div className="h-1/2 w-full relative">
-            <Image src="/slide1.png" alt= "" fill className='object-cover'/>
+            <Image src={data[currentSlide].image} alt= "" fill className='object-cover'/>
         </div>
     </div>
   )
