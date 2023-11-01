8768cce6e08ef0107e46f207372fd6a3ebce4f5b
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-11-01 21:43:58 +0000

    md p-10

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -35,7 +35,7 @@ const Slider = () => {
     <div className=" flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]">
         {/*TEXT CONTAINER*/}
         <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
-            <h1 className="text-5xl uppercase p-4 text-center md:text-6xl xl:text-7xl">{data[currentSlide].title}
+            <h1 className="text-5xl uppercase p-4 md:p-10 text-center md:text-6xl xl:text-7xl">{data[currentSlide].title}
 </h1>
             <button className="bg-red-500 text-white py-4 px-8 ">Order Now</button>
         </div>
