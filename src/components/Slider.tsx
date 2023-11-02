c7de5a9e52019acacb012b65499f3dc7ffe3083c
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-11-02 20:55:41 +0000

    slider complete

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -27,20 +27,20 @@ const data = [
 const Slider = () => {
     const [currentSlide, setCurrentSlide] = useState(0)
 
-    useEffect(() => {
-      const interval = setInterval(() =>setCurrentSlide((prev) => prev === data.length - 1 ? 0 : prev + 1), 2000);
-      return () => clearInterval(interval);
-    },[])
+    // useEffect(() => {
+    //   const interval = setInterval(() =>setCurrentSlide((prev) => prev === data.length - 1 ? 0 : prev + 1), 2000);
+    //   return () => clearInterval(interval);
+    // },[])
   return (
-    <div className=" flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]">
+    <div className=" flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row">
         {/*TEXT CONTAINER*/}
-        <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
+        <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold flex-1">
             <h1 className="text-5xl uppercase p-4 md:p-10 text-center md:text-6xl xl:text-7xl">{data[currentSlide].title}
 </h1>
             <button className="bg-red-500 text-white py-4 px-8 ">Order Now</button>
         </div>
         {/*IMAGE CONTAINER*/}
-        <div className="h-1/2 w-full relative">
+        <div className="h-1/2 w-full relative flex-1">
             <Image src={data[currentSlide].image} alt= "" fill className='object-cover'/>
         </div>
     </div>
